da456b419c9c9ee54a2c30b1c948f4b2a71d288c
mondalankan2007-a11y <mondalankan2007@gmail.com>
2026-09-08 13:35:25 +0530

    feat: add song playback functionality

--- player.js
+++ player.js
@@ -1,26 +1,37 @@
+const { spawn } = require("child_process");
 const fs = require("fs");
 
 // Read all the mp3 files
 const path = "./songs";
-
+let childProcess = null;
 const songs = fs.readdirSync(path).filter((el) => el.endsWith(".mp3"));
 
 console.log(`🎶 Welcome to the Songs App 🎶\n`);
 
 for (let i = 0; i < songs.length; i++) {
-  console.log(`${i + 1}: ${songs[i].split('.')[0]}`);
+  console.log(`${i + 1}: ${songs[i].split(".")[0]}`);
 }
 
 console.log(`\n🎵 Select a number to play the song`);
 
-process.stdin.setEncoding('utf-8')
+process.stdin.setEncoding("utf-8");
 
-process.stdin.on("data", (input)=> {
-    // console.log(+input.toString());
-    const userInput = +input.toString();
-    player(userInput)
-})
+process.stdin.on("data", (input) => {
+  // console.log(+input.toString());
+  const userInput = +input.toString();
+  player(userInput);
+});
 
-function player(userInput){
+function player(userInput) {
+  console.log(`Selected Song: ${songs[userInput - 1]}`);
 
+  if (childProcess) {
+    childProcess.kill();
+  }
+
+  childProcess = spawn("afplay", [`./songs/${songs[userInput - 1]}`]);
+  childProcess.on("close", () => {
+    console.log("Song finished...");
+    process.exit(0);
+  });
 }
